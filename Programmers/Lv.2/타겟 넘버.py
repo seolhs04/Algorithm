5473f2d79428f00767abc7d62b468d328b637ae2
seol <shs00421@gmail.com>
2022-03-15 00:32:53 +0900

    60 타겟 넘버 (bfs 풀이)

--- Programmers/Lv.2/타겟 넘버.py
+++ Programmers/Lv.2/타겟 넘버.py
@@ -1,6 +1,9 @@
+from collections import deque
+
 def solution(numbers, target):
     answer = 0
     
+    # dfs
     def dfs(value, current):
         nonlocal answer
         if current == len(numbers) :
@@ -13,4 +16,18 @@ def solution(numbers, target):
             
     dfs(0, 0)
 
+    return answer
+
+# bfs
+def bfs(numbers, target):
+    queue = deque([[numbers[0],0],[-numbers[0],0]])
+    answer = 0
+    while queue :
+        num, i = queue.popleft()
+        if i+1 == len(numbers) :
+            if num == target : answer+=1
+        else :
+            queue.append([num+numbers[i+1],i+1])
+            queue.append([num-numbers[i+1],i+1])
+    
     return answer
